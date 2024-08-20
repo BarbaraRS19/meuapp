28f892f778b408545d37ac8c230032d32c58c19d
BarbaraRS19 <barbara_sabino@estudante.sesisenai.org.br>
2024-08-20 08:33:19 -0300

    calcatualizada

--- app/calculadora2/index.jsx
+++ app/calculadora2/index.jsx
@@ -1,96 +1,141 @@
-/*import React, { useState } from 'react';
-import {View, StyleSheet, Pressable} from 'react-native';
-import botao from '../../components/botao';
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, SafeAreaView, TouchableOpacity} from 'react-native';
 
+const CalculadoraAtual = () => {
+  const [input, setInput] = useState('');
 
-const App = () => {
-    const [number, onChangeText] = useState('')
-    const [number2, onChangeText2 ] = useState('')
-    const [resultado, setResultado] = useState('')
-    const [operacao,setOperacao] = useState('')
-    const [isNumber2, setIsNumber2] = useState(false)
+  const botao = (value) => {
+    setInput((Input) => Input + value);
+  };
 
-
-    const soma = function (){
-        setResultado(Number(number) + Number(number2))
-      }
-    
-      const subtracao = function (){
-        setResultado(Number(number) - Number(number2) )
-      }
-    
-      const multiplicacao = function (){
-        setResultado(Number(number) * Number(number2))
-      }
-    
-      const divisao = function (){
-        setResultado(Number(number) % Number(number2))
-      }
-
-    const handleSetNumber = (tag,tipo) => {
-        if(tipo = 'number'){
-            if(isNumber2){
-                onChangeText(number + tag)
-                console.log(number)
-            }else{
-                onChangeText2(number + tag)
-                console.log(number2)
-            }
-            }else{
-                setIsNumber2(true)
-                setOperacao(tag)
-            }
-        }
+  const calcular = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch (error) {
+      setInput('Não encontrado!');
     }
+  };
 
-    const handleCalculate = () => {
-        switch(operacao){
-            case '+':
-                soma(setResultado, number, number2)
-                break
-            case '-':
-                subtracao(setResultado, number, number2)
-                break
-            case '*':
-                multiplicacao(setResultado, number, number2)
-                break
-            case '/':
-                divisao(setResultado, number, number2)
-                break
-        }
-        return true
-    }
+  const limpar = () => {
+    setInput('');
+  };
 
-    const handleReset = () => {
-    }
-
-    return (
-        <View style={styles.container}>
-<Botao tag="1" tipo="number" onPress={() => handleSetNumber('1', 'number')}></Botao>
-<Botao tag="2" tipo="number" onPress={() => handleSetNumber('2', 'number')}></Botao>
-<Botao tag="3" tipo="number" onPress={() => handleSetNumber('3', 'number')}></Botao>
-<Botao tag="4" tipo="number" onPress={() => handleSetNumber('4', 'number')}></Botao>
-<Botao tag="5" tipo="number" onPress={() => handleSetNumber('5', 'number')}></Botao>
-<Botao tag="6" tipo="number" onPress={() => handleSetNumber('6', 'number')}></Botao>
-<Botao tag="7" tipo="number" onPress={() => handleSetNumber('7', 'number')}></Botao>
-<Botao tag="8" tipo="number" onPress={() => handleSetNumber('8', 'number')}></Botao>
-<Botao tag="9" tipo="number" onPress={() => handleSetNumber('9', 'number')}></Botao>
-<Botao tag="0" tipo="number" onPress={() => handleSetNumber('0', 'number')}></Botao>
-<Botao tag="+" tipo="operacao" onPress={() => handleSetNumber('+', 'operacao')}></Botao>
-<Botao tag="-" tipo="operacao" onPress={() => handleSetNumber('-', 'operacao')}></Botao>
-<Botao tag="*" tipo="operacao" onPress={() => handleSetNumber('*', 'operacao')}></Botao>
-<Botao tag="/" tipo="operacao" onPress={() => handleSetNumber('/', 'operacao')}></Botao>
-<Botao tag="=" tipo="operacao" onPress={() => handleCalculate()}></Botao>
-<Botao tag="Res" tipo="operacao" onPress={() => handleReset()}></Botao>
+  return (
+    <SafeAreaView style={styles.bodyContainer}>
+      <View style={styles.corpo}>
+        <Text style={styles.texto}>{input}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button} onPress={() => botao('7')}>
+            <Text style={styles.buttonTexto}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => botao('8')}>
+            <Text style={styles.buttonTexto}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => botao('9')}>
+            <Text style={styles.buttonTexto}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonDiferente} onPress={() => botao('*')}>
+            <Text style={styles.buttonTexto}>*</Text>
+          </TouchableOpacity>
         </View>
-)
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button} onPress={() => botao('4')}>
+            <Text style={styles.buttonTexto}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => botao('5')}>
+            <Text style={styles.buttonTexto}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => botao('6')}>
+            <Text style={styles.buttonTexto}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonDiferente} onPress={() => botao('-')}>
+            <Text style={styles.buttonTexto}>-</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button} onPress={() => botao('1')}>
+            <Text style={styles.buttonTexto}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => botao('2')}>
+            <Text style={styles.buttonTexto}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => botao('3')}>
+            <Text style={styles.buttonTexto}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonDiferente} onPress={() => botao('+')}>
+            <Text style={styles.buttonTexto}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.buttonDiferente} onPress={limpar}>
+            <Text style={styles.buttonTexto}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => botao('0')}>
+            <Text style={styles.buttonTexto}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonDiferente} onPress={calcular}>
+            <Text style={styles.buttonTexto}>=</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonDiferente} onPress={() => botao('/')}>
+            <Text style={styles.buttonTexto}>/</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default App;
+const styles = StyleSheet.create({
+  bodyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1C1C1C',
+  },
+  corpo: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#4F4F4F',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 20,
+    justifyContent: 'space-around',
+  },
+  texto: {
+    fontSize: 50,
+    color: '#F8F8FF',
+  },
+  buttonContainer: {
+    width: '50%',
+    padding: 20,
+  },
+  button: { 
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#4F4F4F',
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+    margin: 8,
+  },
+  buttonTexto: {
+    fontSize: 30,
+    color: '#F8F8FF',
+  },
+  buttonDiferente: { 
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#D2691E',
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+    margin: 8,
+  },
+});
 
-const styles = StyleSheet.create({ 
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems:'center'
-    } ,
-  });*/
+export default CalculadoraAtual;
