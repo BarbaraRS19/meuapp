6353712bd3c922daf74b3b5c54fc3c04fce41366
BÁRBARA ROSA SABINO <barbara_sabino@estudante.sesisenai.org.br>
2024-09-03 11:43:34 -0300

    telalog

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,5 +1,62 @@
 import React, { useState } from "react";
 import {View, Text, StyleSheet, TextInput, Pressable} from "react-native";
+import axios from "axios";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default SingUp = () => {
+        const [nome, setNome] = useState('');
+        const [email, setEmail] = useState('');
+        const [senha, setSenha] = useState('');
+        const MudarNome = (nome) => setNome(nome);
+        const MudarEmail = (email) => setEmail(email);
+        const MudarSenha = (senha) => setSenha(senha);
+    
+        const Registro = async () => {
+            if (nome && email && senha) {
+                try {
+                    const response = await axios.post(
+                        'https://taskhub-s37f.onrender.com/auth/signup',
+                        { "name": nome, "email": email, "password": senha }
+                    );
+                    if (response.status === 200){
+                        alert('Usuário criado com sucesso!')
+                    }
+                } catch (error) {
+                    alert('Erro ao criar usuário!', error);
+                }
+            } else {
+                alert('Dados incompletos, revise-os!');
+            }
+        };
+    
+    return <SafeAreaView style={style.container}>
+    <View style={style.body}>
+        <Text style={style.text}>Registre-se</Text>
+        <TextInput 
+                style={style.input}
+                onChangeText={MudarNome}
+                value={nome}
+                placeholder="Nome"
+        />
+        <TextInput 
+                style={style.input}
+                onChangeText={MudarEmail}
+                value={email}
+                placeholder="Email"
+        />
+        <TextInput 
+                style={style.input}
+                onChangeText={MudarSenha}
+                value={senha}
+                secureTextEntry={true}
+                placeholder="Senha"
+        />
+        <Pressable style={style.button} onPress={Registro}>
+        <Text>Concluir</Text>
+        </Pressable>
+    </View>
+    </SafeAreaView>
+}
 
 const style = StyleSheet.create({
     container: {
@@ -8,40 +65,26 @@ const style = StyleSheet.create({
         alignItems: 'center',
         height: '100%',
         width: '100%',
+        backgroundColor: '#f2f3f4'
+    },
+    body: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%',  
     },
     text: {
-        fontSize: 55,
+        fontSize: 50,
         color: '#4F4F4F',
+        marginBottom: 30,
+    },
+    input: {
+        border: '1px solid black',
+        fontSize: 15,
+        marginBottom: 20,
+    },
+    button: {
+        border: '2px solid black',   
     }
 })
-
-export default SingUp = () => {
-    const [text, onChangeText] = React.useState('');
-    const [text2, onChangeText2] = React.useState('');
-    const [text3, onChangeText3] = React.useState('');
-
-    return <View style={style.container}>
-        <Text style={style.text}>Registre-se</Text>
-        <TextInput 
-                style={style.input}
-                onChangeText={onChangeText}
-                value={text}
-                placeholder="Email"
-        />
-        <TextInput 
-                style={style.input}
-                onChangeText={onChangeText2}
-                value={text2}
-                placeholder="Nome"
-        />
-        <TextInput 
-                style={style.input}
-                onChangeText={onChangeText3}
-                value={text3}
-                placeholder="Senha"
-        />
-        <Pressable style={style.button}>
-        <Text>Concluir</Text>
-        </Pressable>
-    </View>
-}
