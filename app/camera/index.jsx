d8473320ac5e65684fa652005ea15f026aa3d44a
BÁRBARA ROSA SABINO <barbara_sabino@estudante.sesisenai.org.br>
2024-10-07 12:26:54 -0300

    cam

--- app/camera/index.jsx
+++ app/camera/index.jsx
@@ -1,12 +1,16 @@
 import { useState, useRef } from 'react';
 import { Image, StyleSheet, Text, View, Button } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function Camera() {
     
     const [permissao, pedirPermissao] = useCameraPermissions();
     const [foto, setFoto] = useState(null);
     const cameraRef = useRef(null)
+    const [ladoCamera, setLadoCamera] = useState('back')
+    const [permissaoSalvar, pedirPermissaoSalvar] = MediaLibrary.usePermissions()
 
     if (!permissao) {
         return <View/>;
@@ -24,20 +28,47 @@ export default function Camera() {
         }
     
     const tirarFoto = async () => {
-        const foto = await cameraRef.current?.takePictureAsync({
+        const fotoBase64 = await cameraRef.current?.takePictureAsync({
             quality: 0.5,
             base64: true
         })
-        setFoto(foto)
-        console.log(foto)
+        setFoto(fotoBase64)
+        console.log(fotoBase64)
+    }
+
+    const inverterLadoCamera = () => {
+      setLadoCamera(ladoCamera == 'back' ? 'front' : 'back')
+    }
+
+    const salvarFoto = async () => {
+      if(permissaoSalvar.status == 'granted'){
+        await pedirPermissaoSalvar()
+      } 
+      MediaLibrary.saveToLibraryAsync(foto.uri)
+      setFoto(null)
     }
 
         return (
-        <CameraView style={style.camera} facing={back} ref={cameraRef}>
+          <View style={style.view}>
+          {
+            foto ?
+            <View style={style.view}>
+            <Button title='descartar imagem' onPress={() => setFoto(null)}/>
+            <Button title='salvar imagem' onPress={salvarFoto}/>
+            <Image style={style.img} source={{uri: foto.uri}}/> 
+            </View>
+            :
+        <CameraView style={style.camera} facing={ladoCamera} ref={cameraRef}
+        barcodeScannerSettings={{
+        barcodeTypes: ["qr"],
+        }}>
         <View style={style.buttonContainer}>
           <Button title='tirar foto' onPress={tirarFoto}/>
+          <Button title='troca lado' onPress={inverterLadoCamera}></Button>
         </View>
         </CameraView>
+          }
+        </View>
           );
     }
 
@@ -58,4 +89,8 @@ const style = StyleSheet.create({
         backgroundColor: 'transparent',
         margin: 64,
       },
+    img: {
+        width: '100%',
+        height: '100%',
+    },
   });
